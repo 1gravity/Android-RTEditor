034e8ce4b7056f329a8da45dcfb7ccca5f9ad508
Emanuel Moecklin <emanuel.moecklin@1gravity.com>
2015-06-02 14:29:13 -0400

    apply auto formatting

--- app/src/main/java/com/onegravity/rteditor/demo/FileHelper.java
+++ app/src/main/java/com/onegravity/rteditor/demo/FileHelper.java
@@ -41,92 +41,88 @@ import java.io.Writer;
 public class FileHelper {
 
     private static final String[][] PICK_DIRECTORY_INTENTS = {
-        { "org.openintents.action.PICK_DIRECTORY", "file://" },   		// OI File Manager (maybe others)
-        { "com.estrongs.action.PICK_DIRECTORY", "file://" },      		// ES File Explorer
-        { Intent.ACTION_PICK, "folder://" },                      		// Blackmoon File Browser (maybe others)
-        { "com.androidworkz.action.PICK_DIRECTORY", "file://" },
-    }; 
+            {"org.openintents.action.PICK_DIRECTORY", "file://"},        // OI File Manager (maybe others)
+            {"com.estrongs.action.PICK_DIRECTORY", "file://"},            // ES File Explorer
+            {Intent.ACTION_PICK, "folder://"},                            // Blackmoon File Browser (maybe others)
+            {"com.androidworkz.action.PICK_DIRECTORY", "file://"},
+    };
 
     /**
      * Tries to open a known file browsers to pick a directory.
+     *
      * @return True if a filebrowser has been found (the result will be in the onActivityResult), False otherwise
      */
-	public static boolean pickDirectory(Activity activity, File startPath, int requestCode) {
-		PackageManager packageMgr = activity.getPackageManager();
-		for (String[] intent : PICK_DIRECTORY_INTENTS) {
+    public static boolean pickDirectory(Activity activity, File startPath, int requestCode) {
+        PackageManager packageMgr = activity.getPackageManager();
+        for (String[] intent : PICK_DIRECTORY_INTENTS) {
             String intentAction = intent[0];
             String uriPrefix = intent[1];
             Intent startIntent = new Intent(intentAction)
-            	.putExtra("org.openintents.extra.TITLE", activity.getString(R.string.save_as))
-            	.setData(Uri.parse(uriPrefix + startPath.getPath()));
+                    .putExtra("org.openintents.extra.TITLE", activity.getString(R.string.save_as))
+                    .setData(Uri.parse(uriPrefix + startPath.getPath()));
 
             try {
-        		if (startIntent.resolveActivity(packageMgr) != null) {
-               		activity.startActivityForResult(startIntent, requestCode);
-	            	return true;
-        		}
-            }
-            catch (ActivityNotFoundException e) {
-        		Log.w(FileHelper.class.getSimpleName(), e.getMessage(), e);
+                if (startIntent.resolveActivity(packageMgr) != null) {
+                    activity.startActivityForResult(startIntent, requestCode);
+                    return true;
+                }
+            } catch (ActivityNotFoundException e) {
+                Log.w(FileHelper.class.getSimpleName(), e.getMessage(), e);
             }
         }
 
         return false;
     }
 
-	public static boolean pickFile(Activity activity, int requestCode) {
-		Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-		intent.setType("file/*");
-		activity.startActivityForResult(intent, requestCode);
-		return true;
-	}
+    public static boolean pickFile(Activity activity, int requestCode) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("file/*");
+        activity.startActivityForResult(intent, requestCode);
+        return true;
+    }
 
-	public static String save(Context context, File outFile, String html) {
+    public static String save(Context context, File outFile, String html) {
         Reader in = null;
         Writer out = null;
-        		
+
         try {
-        	in = new StringReader(html);
+            in = new StringReader(html);
             out = new FileWriter(outFile);
-            
+
             IOUtils.copy(in, out);
 
             return outFile.getAbsolutePath();
-        }
-        catch (IOException ioe) {
+        } catch (IOException ioe) {
             String toastMsg = context.getString(R.string.save_as_failure, ioe.getMessage());
             Toast.makeText(context, toastMsg, Toast.LENGTH_LONG).show();
+        } finally {
+            Helper.closeQuietly(in);
+            Helper.closeQuietly(out);
         }
-        finally {
-        	Helper.closeQuietly(in);
-        	Helper.closeQuietly(out);
-        }
-        
+
         return null;
     }
-    
-	public static String load(Context context, String filePath) {
-    	File inFile = new File(filePath);
+
+    public static String load(Context context, String filePath) {
+        File inFile = new File(filePath);
         Reader in = null;
         Writer out = null;
-        		
+
         try {
-        	in = new FileReader(inFile);
+            in = new FileReader(inFile);
             out = new StringWriter();
-            
+
             IOUtils.copy(in, out);
-            
+
             return out.toString();
-        }
-        catch (IOException ioe) {
+        } catch (IOException ioe) {
             String toastMsg = context.getString(R.string.load_failure_2, ioe.getMessage());
             Toast.makeText(context, toastMsg, Toast.LENGTH_LONG).show();
+        } finally {
+            Helper.closeQuietly(in);
+            Helper.closeQuietly(out);
         }
-        finally {
-        	Helper.closeQuietly(in);
-        	Helper.closeQuietly(out);
-        }
-        
+
         return null;
     }
 
--- app/src/main/java/com/onegravity/rteditor/demo/RichTextEditor.java
+++ app/src/main/java/com/onegravity/rteditor/demo/RichTextEditor.java
@@ -40,45 +40,44 @@ import java.io.File;
 
 public class RichTextEditor extends AppCompatActivity {
 
-	private static final int REQUEST_LOAD_FILE = 1;
-	private static final int REQUEST_SAVE_FILE = 2;
+    private static final int REQUEST_LOAD_FILE = 1;
+    private static final int REQUEST_SAVE_FILE = 2;
 
-	private RTManager mRTManager;
-	private EditText mSubjectField;
-	private RTEditText mRTMessageField;
-	private RTEditText mRTSignatureField;
+    private RTManager mRTManager;
+    private EditText mSubjectField;
+    private RTEditText mRTMessageField;
+    private RTEditText mRTSignatureField;
 
     private boolean mUseDarkTheme;
     private boolean mSplitToolbar;
-    
+
     @Override
-	protected void onCreate(Bundle savedInstanceState) {
-    	// read extras
+    protected void onCreate(Bundle savedInstanceState) {
+        // read extras
         String subject = "";
         String message = "";
         String signature = "";
         if (savedInstanceState == null) {
-	        Intent intent = getIntent();
-	        subject = getStringExtra(intent, "subject");
-	        message = getStringExtra(intent, "message");
-	        signature = getStringExtra(intent, "signature");
-	        mUseDarkTheme = intent.getBooleanExtra("mUseDarkTheme", false);
-	        mSplitToolbar = intent.getBooleanExtra("mSplitToolbar", false);
-        }
-        else {
-	        subject = savedInstanceState.getString("subject", "");
-	        message = savedInstanceState.getString("message", "");
-	        signature = savedInstanceState.getString("signature", "");
-	        mUseDarkTheme = savedInstanceState.getBoolean("mUseDarkTheme", false);
-	        mSplitToolbar = savedInstanceState.getBoolean("mSplitToolbar", false);
+            Intent intent = getIntent();
+            subject = getStringExtra(intent, "subject");
+            message = getStringExtra(intent, "message");
+            signature = getStringExtra(intent, "signature");
+            mUseDarkTheme = intent.getBooleanExtra("mUseDarkTheme", false);
+            mSplitToolbar = intent.getBooleanExtra("mSplitToolbar", false);
+        } else {
+            subject = savedInstanceState.getString("subject", "");
+            message = savedInstanceState.getString("message", "");
+            signature = savedInstanceState.getString("signature", "");
+            mUseDarkTheme = savedInstanceState.getBoolean("mUseDarkTheme", false);
+            mSplitToolbar = savedInstanceState.getBoolean("mSplitToolbar", false);
         }
 
         // set theme
-    	setTheme(mUseDarkTheme ? R.style.ThemeDark : R.style.ThemeLight);
+        setTheme(mUseDarkTheme ? R.style.ThemeDark : R.style.ThemeLight);
 
         super.onCreate(savedInstanceState);
-        
-    	// set layout
+
+        // set layout
         setContentView(mSplitToolbar ? R.layout.rte_demo_2 : R.layout.rte_demo_1);
 
         // initialize rich text manager
@@ -90,201 +89,194 @@ public class RichTextEditor extends AppCompatActivity {
         // register toolbar 0 (if it exists)
         HorizontalRTToolbar rtToolbar0 = (HorizontalRTToolbar) findViewById(R.id.rte_toolbar);
         if (rtToolbar0 != null) {
-	        mRTManager.registerToolbar(toolbarContainer, rtToolbar0);
+            mRTManager.registerToolbar(toolbarContainer, rtToolbar0);
         }
 
         // register toolbar 1 (if it exists)
         HorizontalRTToolbar rtToolbar1 = (HorizontalRTToolbar) findViewById(R.id.rte_toolbar_character);
         if (rtToolbar1 != null) {
-	        mRTManager.registerToolbar(toolbarContainer, rtToolbar1);
+            mRTManager.registerToolbar(toolbarContainer, rtToolbar1);
         }
 
         // register toolbar 2 (if it exists)
         HorizontalRTToolbar rtToolbar2 = (HorizontalRTToolbar) findViewById(R.id.rte_toolbar_paragraph);
         if (rtToolbar2 != null) {
-	        mRTManager.registerToolbar(toolbarContainer, rtToolbar2);
+            mRTManager.registerToolbar(toolbarContainer, rtToolbar2);
         }
-        
+
         // set subject
         mSubjectField = (EditText) findViewById(R.id.subject);
         mSubjectField.setText(subject);
 
         // register editor 1
         mRTMessageField = (RTEditText) findViewById(R.id.rtEditText_1);
-       	mRTManager.registerEditor(mRTMessageField, true);
-       	mRTMessageField.setRichTextEditing(true, message);
-       	mRTMessageField.requestFocus();
-       	
-       	// register editor 2
-       	mRTSignatureField = (RTEditText) findViewById(R.id.rtEditText_2);
-       	mRTManager.registerEditor(mRTSignatureField, true);
-       	mRTSignatureField.setRichTextEditing(true, signature);
-	}
+        mRTManager.registerEditor(mRTMessageField, true);
+        mRTMessageField.setRichTextEditing(true, message);
+        mRTMessageField.requestFocus();
+
+        // register editor 2
+        mRTSignatureField = (RTEditText) findViewById(R.id.rtEditText_2);
+        mRTManager.registerEditor(mRTSignatureField, true);
+        mRTSignatureField.setRichTextEditing(true, signature);
+    }
 
     private String getStringExtra(Intent intent, String key) {
-    	String s = intent.getStringExtra(key);
-    	return s == null ? "" : s;
+        String s = intent.getStringExtra(key);
+        return s == null ? "" : s;
     }
 
     @Override
     public void onDestroy() {
-    	super.onDestroy();
-    	if (mRTManager != null) {
-    		mRTManager.onDestroy( false );
-    	}
+        super.onDestroy();
+        if (mRTManager != null) {
+            mRTManager.onDestroy(false);
+        }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-    	super.onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
 
-    	mRTManager.onSaveInstanceState(outState);
-        
+        mRTManager.onSaveInstanceState(outState);
+
         String subject = mSubjectField.getText().toString();
-    	if (subject != null) outState.putString("subject", subject);
+        if (subject != null) outState.putString("subject", subject);
 
-    	String message = mRTMessageField.getText(RTFormat.HTML);
-    	if (message != null) outState.putString("message", message);
+        String message = mRTMessageField.getText(RTFormat.HTML);
+        if (message != null) outState.putString("message", message);
 
-    	String signature = mRTSignatureField.getText(RTFormat.HTML);
-    	if (signature != null) outState.putString("signature", signature);
-    	
-    	outState.putBoolean("mUseDarkTheme", mUseDarkTheme);
-    	outState.putBoolean("mSplitToolbar", mSplitToolbar);
+        String signature = mRTSignatureField.getText(RTFormat.HTML);
+        if (signature != null) outState.putString("signature", signature);
+
+        outState.putBoolean("mUseDarkTheme", mUseDarkTheme);
+        outState.putBoolean("mSplitToolbar", mSplitToolbar);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    	if (mRTManager != null && mRTManager.onActivityResult(requestCode, resultCode, data)) {
-    		return;
-    	}
-    	if (resultCode == Activity.RESULT_OK && data != null && data.getData() != null && data.getData().getPath() != null) {
+        if (mRTManager != null && mRTManager.onActivityResult(requestCode, resultCode, data)) {
+            return;
+        }
+        if (resultCode == Activity.RESULT_OK && data != null && data.getData() != null && data.getData().getPath() != null) {
             String filePath = data.getData().getPath();
 
             if (requestCode == REQUEST_SAVE_FILE) {
-            	/*
+                /*
             	 * Save file.
             	 * 
             	 * Of course this is a hack but since this is just a demo
             	 * to show how to integrate the rich text editor this is ok ;-)
             	 */
-            	
-        		// write subject
-        		File targetFile = MediaUtils.createUniqueFile(new File(filePath), "subject.html", true);
-        		String fileName = FileHelper.save(this, targetFile, mSubjectField.getText().toString());
-        		
-        		// write message
-        		targetFile = new File( targetFile.getAbsolutePath().replace("subject_", "message_") );
-        		FileHelper.save(this, targetFile, mRTMessageField.getText(RTFormat.HTML));
 
-        		// write signature
-        		targetFile = new File( targetFile.getAbsolutePath().replace("message_", "signature_") );
-        		FileHelper.save(this, targetFile, mRTSignatureField.getText(RTFormat.HTML));
+                // write subject
+                File targetFile = MediaUtils.createUniqueFile(new File(filePath), "subject.html", true);
+                String fileName = FileHelper.save(this, targetFile, mSubjectField.getText().toString());
 
-        		if (fileName != null) {
-        			String toastMsg = getString(R.string.save_as_success, fileName);
-        			Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
-        		}
-        	}
+                // write message
+                targetFile = new File(targetFile.getAbsolutePath().replace("subject_", "message_"));
+                FileHelper.save(this, targetFile, mRTMessageField.getText(RTFormat.HTML));
 
-            else if (requestCode == REQUEST_LOAD_FILE) {
+                // write signature
+                targetFile = new File(targetFile.getAbsolutePath().replace("message_", "signature_"));
+                FileHelper.save(this, targetFile, mRTSignatureField.getText(RTFormat.HTML));
+
+                if (fileName != null) {
+                    String toastMsg = getString(R.string.save_as_success, fileName);
+                    Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+                }
+            } else if (requestCode == REQUEST_LOAD_FILE) {
         		/*
         		 * Load File
         		 * 
             	 * A hack, I know ...
         		 */
-            	
-            	if (filePath.contains("message_")) {
-            		filePath = filePath.replace("message_", "subject_");
-            	}
-            	else if (filePath.contains("signature_")) {
-            		filePath = filePath.replace("signature_", "subject_");
-            	}
 
-            	if (filePath.contains("subject_")) {
-            		// load subject
-            		String s = FileHelper.load(this, filePath);
-            		mSubjectField.setText(s);
+                if (filePath.contains("message_")) {
+                    filePath = filePath.replace("message_", "subject_");
+                } else if (filePath.contains("signature_")) {
+                    filePath = filePath.replace("signature_", "subject_");
+                }
 
-            		// load message
-            		filePath = filePath.replace("subject_", "message_");
-            		s = FileHelper.load(this, filePath);
-            		mRTMessageField.setRichTextEditing(true, s);
-            		
-            		// load signature
-            		filePath = filePath.replace("message_", "signature_");
-            		s = FileHelper.load(this, filePath);
-            		mRTSignatureField.setRichTextEditing(true, s);
-            	}
-            	else {
+                if (filePath.contains("subject_")) {
+                    // load subject
+                    String s = FileHelper.load(this, filePath);
+                    mSubjectField.setText(s);
+
+                    // load message
+                    filePath = filePath.replace("subject_", "message_");
+                    s = FileHelper.load(this, filePath);
+                    mRTMessageField.setRichTextEditing(true, s);
+
+                    // load signature
+                    filePath = filePath.replace("message_", "signature_");
+                    s = FileHelper.load(this, filePath);
+                    mRTSignatureField.setRichTextEditing(true, s);
+                } else {
                     Toast.makeText(this, R.string.load_failure_1, Toast.LENGTH_LONG).show();
-            	}
+                }
             }
 
-    	}
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.main_menu, menu);
-	    return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
-    
-	@Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-		// configure theme item
-		MenuItem item = menu.findItem(R.id.theme);
-		item.setTitle(mUseDarkTheme ? R.string.menu_light_theme : R.string.menu_dark_theme);
-		
-		// configure split toolbar item
-		item = menu.findItem(R.id.split_toolbar);
-		item.setTitle(mSplitToolbar ? R.string.menu_single_toolbar : R.string.menu_split_toolbar);
 
-	    return true;
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // configure theme item
+        MenuItem item = menu.findItem(R.id.theme);
+        item.setTitle(mUseDarkTheme ? R.string.menu_light_theme : R.string.menu_dark_theme);
+
+        // configure split toolbar item
+        item = menu.findItem(R.id.split_toolbar);
+        item.setTitle(mSplitToolbar ? R.string.menu_single_toolbar : R.string.menu_split_toolbar);
+
+        return true;
     }
-    
-	@Override
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-		int itemId = item.getItemId();
-		if (itemId == R.id.load) {
-			FileHelper.pickFile(this, REQUEST_LOAD_FILE);
-			return true;
-		}
-		else if (itemId == R.id.save) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.load) {
+            FileHelper.pickFile(this, REQUEST_LOAD_FILE);
+            return true;
+        } else if (itemId == R.id.save) {
 			/*
 			 * Note that you need a third party file explorer that
 			 * supports a pick directory Intent (like ES File Explorer or any
 			 * Open Intent file explorer).
 			 */
-			File targetDir = getExternalFilesDir(null);
-			FileHelper.pickDirectory(this, targetDir, REQUEST_SAVE_FILE);
-			return true;
-		}
-		else if (itemId == R.id.theme) {
-			mUseDarkTheme = ! mUseDarkTheme;
-			restart();
-			return true;
-		}
-		else if (itemId == R.id.split_toolbar) {
-			mSplitToolbar = ! mSplitToolbar;
-			restart();
-			return true;
-		}
-		return false;
+            File targetDir = getExternalFilesDir(null);
+            FileHelper.pickDirectory(this, targetDir, REQUEST_SAVE_FILE);
+            return true;
+        } else if (itemId == R.id.theme) {
+            mUseDarkTheme = !mUseDarkTheme;
+            restart();
+            return true;
+        } else if (itemId == R.id.split_toolbar) {
+            mSplitToolbar = !mSplitToolbar;
+            restart();
+            return true;
+        }
+        return false;
     }
-	
-	private void restart() {
+
+    private void restart() {
         String subject = mSubjectField.getText().toString();
-    	String message = mRTMessageField.getText(RTFormat.HTML);
-    	String signature = mRTSignatureField.getText(RTFormat.HTML);
-		Intent intent = new Intent(this, getClass())
-			.putExtra("mUseDarkTheme", mUseDarkTheme)
-			.putExtra("mSplitToolbar", mSplitToolbar)
-			.putExtra("subject", subject)
-			.putExtra("message", message)
-			.putExtra("signature", signature);
-		startActivity(intent);
-		finish();
-	}
+        String message = mRTMessageField.getText(RTFormat.HTML);
+        String signature = mRTSignatureField.getText(RTFormat.HTML);
+        Intent intent = new Intent(this, getClass())
+                .putExtra("mUseDarkTheme", mUseDarkTheme)
+                .putExtra("mSplitToolbar", mSplitToolbar)
+                .putExtra("subject", subject)
+                .putExtra("message", message)
+                .putExtra("signature", signature);
+        startActivity(intent);
+        finish();
+    }
 }
