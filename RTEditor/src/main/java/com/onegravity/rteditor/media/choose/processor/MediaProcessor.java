50f5c035a8dd1da7783cb8783d56d87b8698bc17
Emanuel Moecklin <contact@1gravity.com>
2015-09-04 00:42:38 -0400

    Use the standard Android HttpURLConnection instead of deprecated Apache classes

--- RTEditor/src/main/java/com/onegravity/rteditor/media/choose/processor/MediaProcessor.java
+++ RTEditor/src/main/java/com/onegravity/rteditor/media/choose/processor/MediaProcessor.java
@@ -28,15 +28,15 @@ import com.onegravity.rteditor.api.media.RTImage;
 import com.onegravity.rteditor.api.media.RTVideo;
 
 import org.apache.commons.io.FilenameUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.commons.io.IOUtils;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public abstract class MediaProcessor implements Runnable {
 
@@ -114,15 +114,19 @@ public abstract class MediaProcessor implements Runnable {
     }
 
     private InputStream downloadFile(String sourceFile) {
-        HttpClient client = new DefaultHttpClient();
-        HttpGet getRequest = new HttpGet(sourceFile);
-
         InputStream in = null;
         try {
-            HttpResponse response = client.execute(getRequest);
-            in = response.getEntity().getContent();
+            URL url = new URL(sourceFile);
+            final HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            in = new BufferedInputStream(urlConnection.getInputStream()) {
+                public void close() throws IOException {
+                    super.close();
+                    urlConnection.disconnect();
+                }
+            };
         } catch (Exception e) {
             Log.e(getClass().getSimpleName(), e.getMessage(), e);
+            IOUtils.closeQuietly(in);
         }
 
         return in;
