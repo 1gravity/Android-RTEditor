5cf3b5fc8aad6ea80317ddbe9267a41afc2e8bda
Emanuel Moecklin <contact@1gravity.com>
2016-06-13 10:16:20 -0400

    more meaningful log output message if the assets.index file is missing

--- RTEditor/src/main/java/com/onegravity/rteditor/fonts/AssetIndex.java
+++ RTEditor/src/main/java/com/onegravity/rteditor/fonts/AssetIndex.java
@@ -49,7 +49,7 @@ abstract class AssetIndex {
                     mAssetIndex.add(line);
                 }
             } catch (final IOException e) {
-                Log.e(AssetIndex.class.getSimpleName(), e.getMessage(), e);
+                Log.w(AssetIndex.class.getSimpleName(), "The assets.index file could not be read. If you want to use your own fonts, please copy the fonts to the assets folder and the build code to generate the assets.index file into your build.gradle (for more details consult the readme, chapter fonts)", e);
             } finally {
                 IOUtils.closeQuietly(in);
                 IOUtils.closeQuietly(reader);
