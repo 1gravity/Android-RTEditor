f073979834f4cdf63f7254e248263f4c8dad071a
Emanuel Moecklin <emanuel.moecklin@gmail.com>
2022-02-08 14:41:01 -0500

    fix: the library can be built without the OSS keys

--- RTEditor/build.gradle.kts
+++ RTEditor/build.gradle.kts
@@ -110,8 +110,8 @@ afterEvaluate {
                     url = getRepositoryUrl()
                     // credentials are stored in ~/.gradle/gradle.properties with ~ being the path of the home directory
                     credentials {
-                        username = props["ossUsername"]?.toString() ?: throw IllegalStateException("ossUsername not found")
-                        password = props["ossPassword"]?.toString() ?: throw IllegalStateException("ossPassword not found")
+                        username = props["ossUsername"]?.toString() ?: "ossUsername not found"
+                        password = props["ossPassword"]?.toString() ?: "ossPassword not found"
                     }
                 }
             }
@@ -164,12 +164,9 @@ afterEvaluate {
 
             // 3. sign the artifacts
             signing {
-                val signingKeyId = props["signingKeyId"]?.toString()
-                        ?: throw IllegalStateException("signingKeyId not found")
-                val signingKeyPassword = props["signingKeyPassword"]?.toString()
-                        ?: throw IllegalStateException("signingKeyPassword not found")
-                val signingKey = props["signingKey"]?.toString()
-                        ?: throw IllegalStateException("signingKey not found")
+                val signingKeyId = props["signingKeyId"]?.toString() ?: "signingKeyId not found"
+                val signingKeyPassword = props["signingKeyPassword"]?.toString() ?: "signingKeyPassword not found"
+                val signingKey = props["signingKey"]?.toString() ?: "signingKey not found"
                 useInMemoryPgpKeys(signingKeyId, signingKey, signingKeyPassword)
                 sign(publishing.publications.getByName(publicationName))
             }
